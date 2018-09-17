36b9a640477c2233901965650d25deb543cb5cf9
Luca Strebel <luca.strebel@gmx.net>
2018-09-17 14:25:35 +0200

    added TOTP

--- TwoFactorAuthentication/index.js
+++ TwoFactorAuthentication/index.js
@@ -1,22 +1,24 @@
 // server lib
 var express = require('express');
-
+var speakeasy = require('speakeasy');
 var QRCode = require('qrcode');
 var bodyParser = require('body-parser')
 
 var app = express();
-
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+var speakeasy = require('speakeasy');
+var secret = speakeasy.generate_key();
 
-const secret = 'N5NFE4R6MJLGM3LNFQUVIYJIHFHG22KD';
+const admin_username = "Yaca";
+const admin_password = "Luis";
 
 app.get('/get', (req, res) => {
     /*var secret = speakeasy.generateSecret({length: 20});
     console.log(secret.base32); // Save this value to your DB for the user*/
-    QRCode.toDataURL(secret, function(err, image_data) {
+    QRCode.toDataURL(secret.otpauth_url, function(err, image_data) {
         res.send('<img src="' + image_data + '" />'); // A data URI for the QR code image
     });
 });
@@ -26,8 +28,16 @@ app.get('/check', (req, res) => {
 });
 
 app.post('/checkp', (req, res) => {
-    //{ username, password, token } = req.body;
-    res.send('AHAHAH');
+    var { username, password, token } = req.body;    
+    var verified = false;
+    if(username == admin_username && password == admin_password){
+        verified = speakeasy.totp.verify({
+            secret: secret.base32,
+            encoding: 'base32',
+            token: token
+        });
+    }   
+    res.send(`Verified: ${verified}`);
 })
 
 app.listen(3000, function () {
