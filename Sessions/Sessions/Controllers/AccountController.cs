7559b20befe061d0df491d2eaaeec9ebd6fd90e7
kraemery <yannis.kraemer@roche.com>
2018-10-29 14:39:44 +0100

    Added sessions

---
+++ Sessions/Sessions/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace Sessions.Controllers
+{
+  public class AccountController : Controller
+  {
+    // GET: Account
+    public ActionResult Index()
+    {
+      return RedirectToAction(nameof(Login));
+    }
+
+    [HttpGet]
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(string username, string password)
+    {
+      if (CheckCredentials(username, password))
+      {
+        return RedirectToAction(nameof(HomeController.Index), nameof(HomeController));
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    private bool CheckCredentials(string username, string password)
+    {
+      return username == "user" && password == "pw";
+    }
+  }
+}
