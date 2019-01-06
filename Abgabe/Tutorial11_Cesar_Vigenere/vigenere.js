1bdd72c278a51b369a60d7b1f66a2769c1a951dd
Luca Strebel <luca.strebel@gmx.net>
2019-01-06 20:48:15 +0100

    more stuff

--- Abgabe/Tutorial11_Cesar_Vigenere/vigenere.js
+++ Abgabe/Tutorial11_Cesar_Vigenere/vigenere.js
@@ -1,20 +1,20 @@
-function vigenere(key, text, decode){
+function vigenere(key, text, decode) {
 	key = key.split('').map(a => a.charCodeAt(0)).map(alphabetPosition);
-	if(!key.every(a => a))
+	if (!key.every(a => a))
 		return console.log('invalid key');
 	key = key.map(a => a.pos);
 	return text.split('').map(a => a.charCodeAt(0)).map((a, i) => {
-	var base = alphabetPosition(a);
-	if(!base)
-		return a;	
-	base = base.base;
-	return base + ((26 + a - base + key[i % key.length] * (decode ? -1 : 1)) % 26)
-}).map(a => String.fromCharCode(a)).join('')
+		var base = alphabetPosition(a);
+		if (!base)
+			return a;
+		base = base.base;
+		return base + ((26 + a - base + key[i % key.length] * (decode ? -1 : 1)) % 26)
+	}).map(a => String.fromCharCode(a)).join('')
 
-  function alphabetPosition(charCode) {
-	 var base = charCode < 91 && charCode >= 65 ? 65 : charCode >= 97 && charCode < 123 ? 97 : false;
-     if(!base)
-       return false;
-	 return {base: base, pos: charCode - base}	
-  }
+	function alphabetPosition(charCode) {
+		var base = charCode < 91 && charCode >= 65 ? 65 : charCode >= 97 && charCode < 123 ? 97 : false;
+		if (!base)
+			return false;
+		return { base: base, pos: charCode - base }
+	}
 }
